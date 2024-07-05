180b9affc12962cce8dad6c59358f691b29f639b
Jaikishan Gupta <jaikishangupta754@gmail.com>
2024-07-05 12:36:44 +0530

    useLoaderData_Hook

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -13,6 +13,7 @@ import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -41,7 +42,12 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />}></Route>
       <Route path="about" element={<About />}></Route>
       <Route path="contact" element={<Contact />}></Route>
-      <Route path="/user/:userid" element={<User />}></Route>
+      <Route path="user/:userid" element={<User />}></Route>
+      <Route
+        loader={githubInfoLoader}
+        path="github"
+        element={<Github />}
+      ></Route>
     </Route>
   )
 );
